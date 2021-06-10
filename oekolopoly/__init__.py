7cbb0a5fcf01c4675aca0a30360da6428bbdf6de
Ralitsa <71445512+cherrisimo@users.noreply.github.com>
2021-06-10 13:42:01 +0300

    Update __init__.py

--- oekolopoly/__init__.py
+++ oekolopoly/__init__.py
@@ -1,4 +1,11 @@
 from gym.envs.registration import register
+#import gym
+
+#env_dict = gym.envs.registration.registry.env_specs.copy()
+#for env in env_dict:
+#    if 'Oekolopoly-v0' in env:
+#         print("Remove {} from registry".format(env))
+#         del gym.envs.registration.registry.env_specs[env]
 
 register(
     id='Oekolopoly-v0',
